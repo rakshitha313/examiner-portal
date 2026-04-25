8108c1e905be0346e67542c70fe76305bf48cdd3
Rakshitha <rakshithabr510@gmail.com>
2026-04-25 23:24:41 +0530

    gunicorn added

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&9z0nkc^y=!i)#hnzaa1tb)(ye3+q@^@-1y+zt$r-q#2sp^af@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
